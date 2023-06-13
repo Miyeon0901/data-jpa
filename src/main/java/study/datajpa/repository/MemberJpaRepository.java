f7dad12814090fc09e5d393759eaa22f4963a916
mimi <kmy930901@naver.com>
2023-06-13 22:34:24 +0900

    섹션3. 쿼리 메소드 기능 - 순수 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -54,4 +54,18 @@ public class MemberJpaRepository {
                 .setParameter("username", username)
                 .getResultList();
     }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class) // sorting 이 불필요.
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
